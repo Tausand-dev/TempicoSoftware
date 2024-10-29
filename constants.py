84ef18e64ca549077735124f82d1d9efe2734f88
Joan Amaya <j.amayab@uniandes.edu.co>
2024-10-29 13:50:52 -0500

    Changes in folders

--- constants.py
+++
@@ -1,6 +0,0 @@
-#File of constants for scripts
-ICON_LOCATION="./Sources/tausand_small.ico"
-VERSION="1.0.0"
-BANNER="./Sources/splash.png"
-#Create the taskbarIcon for windows with ctypes
-APPID="tempico.tempico.01"
--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-def indexChangeStartChannel(self):
-        if self.comboBoxStartChannel.currentIndex()==self.comboBoxStopChannel.currentIndex()+1:
-            self.comboBoxStartChannel.setCurrentIndex(self.oldStartChannelIndex)
-        else:
-            self.oldStartChannelIndex=self.comboBoxStartChannel.currentIndex()
