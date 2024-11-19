5de8613beb7eac299f5576fd31fead23b68929a3
Joan Amaya <j.amayab@uniandes.edu.co>
2024-11-19 13:45:16 -0500

    Changes in .ico

--- TempicoSoftware/constants.py
+++ TempicoSoftware/constants.py
@@ -1,5 +1,5 @@
 #File of constants for scripts
-ICON_LOCATION="./Sources/abacus_small.ico"
+ICON_LOCATION="./Sources/tausand_small.ico"
 VERSION="1.0.0"
 BANNER="./Sources/splash.png"
 #Create the taskbarIcon for windows with ctypes
