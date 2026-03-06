b759011a8f46064a494cdb1438dd8dacd3beaab6
Joan Sebastian Amaya Bohórquez <98501917+JoanAmaya@users.noreply.github.com>
2026-03-06 17:10:25 -0500

    Update constants.py

--- src/Utils/constants.py
+++ src/Utils/constants.py
@@ -1,7 +1,7 @@
 #File of constants for scripts
 ICON_LOCATION="./Sources/tausand_small.ico"
 VERSION="2.0.0"
-PYTEMPICO_VERSION="2.0.1"
+PYTEMPICO_VERSION="2.0.2"
 BANNER="./Sources/splash.png"
 #Create the taskbarIcon for windows with ctypes
 APPID="tempico.tempico.01"
